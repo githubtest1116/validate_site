406ba7318b3035cd8c5ec05569c207b63b1c86d1
githubtest1116 <githubtest1116@gmail.com>
2018-01-18 11:53:31 +0000

    site add

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -1,4 +1,10 @@
 class SampleController < ApplicationController
   def index
   end
+  
+  def static_image
+  end
+  
+  def variable_image
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   #トップページ
   root to: 'sample#index'
   
+  get 'static_image', to: 'sample#static_image'
+  get 'variable_image', to: 'sample#variable_image'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
